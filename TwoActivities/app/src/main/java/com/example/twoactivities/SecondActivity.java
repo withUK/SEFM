3b850f064314701db0f99b7c9b7c98603cf4687c
withUK <peter.withington@live.co.uk>
2021-05-28 16:58:02 +0100

    1.3 Implement lifecycle callbacks in SecondActivity
    Added overrides to 2nd Activity

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
 
     private EditText mReply;
@@ -23,6 +25,42 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     public void returnReply(View view) {
