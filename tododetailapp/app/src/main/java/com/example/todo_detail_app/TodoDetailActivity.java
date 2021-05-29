9ebd6ddbd019f42a1cdfe72ea8ad75ac78f965ca
withUK <peter.withington@live.co.uk>
2021-05-29 20:57:27 +0100

    Todo Details App - created and views added

---
+++ tododetailapp/app/src/main/java/com/example/todo_detail_app/TodoDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.todo_detail_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+    }
+}
