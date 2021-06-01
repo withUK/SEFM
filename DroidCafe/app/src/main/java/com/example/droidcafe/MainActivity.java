3c7c87defb6b4d65516df1c90a3f9d1e61cb6ea1
withUK <peter.withington@live.co.uk>
2021-06-01 16:02:23 +0100

    Start the new project
    Created project and removed default buttons from template.
    Renamed id of TextView  with margin of 8dp.

--- DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -17,7 +17,6 @@ import com.example.droidcafe.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,31 +72,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public void displayToast(String message) {
-        Toast.makeText(getApplicationContext(), message,
-                Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Shows a message that the donut image was clicked.
-     */
-    public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
-    }
-
-    /**
-     * Shows a message that the ice cream sandwich image was clicked.
-     */
-    public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
-    }
-
-    /**
-     * Shows a message that the froyo image was clicked.
-     */
-    public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
     }
 }
