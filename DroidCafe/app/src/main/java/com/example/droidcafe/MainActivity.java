2d0ba19e17ff4e8eec06d06dcef26ea1da60d4f9
withUK <peter.withington@live.co.uk>
2021-06-01 16:58:30 +0100

    Task 3: Change the floating action button
    3.1 Add a new icon
    3.2 Add an Activity
    3.3 Change the action
    Added onClick within the onClickListener within the MainActivity class

--- DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.droidcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -40,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
             }
         });
     }
